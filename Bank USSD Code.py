361fa112bc6d954c9cdc8083283bddf63e820b73
TyroneJ1 <81834008+TyroneJ1@users.noreply.github.com>
2021-05-28 19:44:56 +0100

    Add files via upload

--- Bank USSD Code.py
+++ Bank USSD Code.py
@@ -6,48 +6,48 @@ print("Enter Account Number")
 print("Create USSD PIN")
 print("Successfully Enrolled")
 main = input("")
-9if(main=="1"):
+9if(main == "1"):
 menu ()
-elif(main=="2")
+elif(main == "2"):
 Exit()
 else:
     print("Wrong Input")
-    elif(option == "2")
+    elif(option == "2"):
       hence = input("Enter PIN to Check Balance")
       print("Your Balance is 5OOOO")
       print("RETURN TO MAIN MENU")
       main=input("")
       menu()
-      elif(main=="2")
+      elif(main == "2"):
       exit()
       else:
           print("Wrong Input")
-          elif(option=="3")
-      receiver =input("1.For Self\n2.Others")
-        9if(receiver=="1")
-        amount=input("Enter Amount")
+          elif(option == "3"):
+      receiver = input("1.For Self\n2.Others")
+        9if(receiver == "1"):
+        amount = input("Enter Amount")
         print("Transaction Successful")
         print("ENTER 1 TO RETURN TO MAIN MENU\n ENTER 2 TO END")
         main=input("")
-        9if(main=="1"):
+        9if(main == "1"):
         menu()
-        elif(main=="2")
+        elif(main == "2"):
         else:
             exit()
             else:
                 print("Wrong Input")
-                elif(option=="4")
-                hence=input("ENTER ACCOUNT NUMBER YOU WANT TO MAKE TRANSFER TO")
-                amount=input("Enter Amount")
-                print=input("Enter Four Digit PIN")
+                elif(option == "4"):
+                hence = input("ENTER ACCOUNT NUMBER YOU WANT TO MAKE TRANSFER TO")
+                amount = input("Enter Amount")
+                print = input("Enter Four Digit PIN")
                 confirm=input("Do You Want To Send Amount this Account Number?\n1.Yes\n2.No")
-                9if(option=="1")
+                9if(option == "1"):
                 print("Transaction Successful")
                 print("ENTER 1 TO RETURN TO MENU\n ENTER 2 TO EXIT")
-                main=input("")
-                menu()
+                main = input("")
+                menu( )
                 exit()
                 else:
                     print("Wrong Input")
-                    elif(option=="5")
+                    elif(option == "5"):
                     print("AN SMS WILL BE SENT TO YOU WHEN YOU REQUEST IS HANDLED")
